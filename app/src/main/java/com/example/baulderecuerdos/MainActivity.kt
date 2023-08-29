b1a7965599b57cb64afee8774cee9ef7f8e2c708
PatricioCataldo <pato.elias15@gmail.com>
2023-08-29 17:23:49 -0400

    1st commit

--- app/src/main/java/com/example/baulderecuerdos/MainActivity.kt
+++ app/src/main/java/com/example/baulderecuerdos/MainActivity.kt
@@ -2,30 +2,17 @@ package com.example.baulderecuerdos
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.baulderecuerdos.R.layout.activity_main
 import com.example.baulderecuerdos.ui.theme.BaulDeRecuerdosTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            BaulDeRecuerdosTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(activity_main)
     }
 }
 
